15fa7b90588f49372ce343e4875bf650e158781f
EC2 Default User <ec2-user@ip-172-31-46-163.ap-southeast-2.compute.internal>
2024-04-17 05:10:28 +0000

    Basic program

--- backup.py
+++ backup.py
@@ -1,7 +1,38 @@
 #!/usr/bin/python3
 
-def main():
-    print('Hello, World!')
+import sys
+import os
+import pathlib
+import shutil
+from datetime import datetime
+from backupcfg import jobs, dstPath
 
+def main():
+    dateTimeStamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    argCount = len(sys.argv)
+    if argCount != 2:
+        print("ERROR: jobname missing from command line")
+    else:
+        jobName = sys.argv[1]
+        if jobName not in jobs:
+            print(f"ERROR: jobname {jobName} not defined.")
+        else:   
+            srcPath = jobs[jobName]
+            if not os.path.exists(srcPath):
+                print(f"ERROR: Source path {srcPath} does not exist.")
+            else:
+                if not os.path.exists(dstPath):
+                    print(f"ERROR: Destination path {dstPath} does not exist.")
+                else: 
+                     srcDetails = pathlib.PurePath(srcPath)
+                     
+                     dstLoc = dstPath + "/" + srcDetails.name + "-" + dateTimeStamp
+                     
+                     if pathlib.Path(srcPath).is_dir():
+                        shutil.copytree(srcPath, dstLoc)
+                     else:
+                        shutil.copy2(srcPath, dstLoc)
+                     pass
+                
 if __name__ == '__main__':
     main()
