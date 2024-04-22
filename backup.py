7c815b1ed6400df914e459da6216330184d95628
Kassie <kassie.owen@live.com>
2024-04-22 04:28:29 +0000

    test

--- backup.py
+++ backup.py
@@ -1,38 +1,92 @@
 #!/usr/bin/python3
+"""
+Script title:
+Author: Kassandra Owen
+Author email: kassie.owen@live.com
+Version control:
+
+"""
 
 import sys
 import os
 import pathlib
 import shutil
+import smtplib
 from datetime import datetime
-from backupcfg import jobs, dstPath
+from backupcfg import jobs, dstPath, logPath, smtp
 
+def sendEmail(message, dateTimeStamp): 
+    """ 
+    Send an email message to the specified recipient. 
+    Parameters: 
+    message (string): message to send. 
+    dateTimeStamp (string): Date and time when program was run. 
+    """ 
+    # create email message 
+    email = 'To: ' + smtp["recipient"] + '\n' + 'From: ' + smtp["sender"] + '\n' + 'Subject: Backup Error\n\n' + message + '\n' 
+   
+    # connect to email server and send email
+    try:
+        smtp_server = smtplib.SMTP(smtp["server"], smtp["port"])
+        smtp_server.ehlo()
+        smtp_server.starttls()
+        smtp_server.ehlo()
+        smtp_server.login(smtp["user"], smtp["password"])
+        smtp_server.sendmail(smtp["sender"], smtp["recipient"], email)
+        smtp_server.close()
+    except Exception as e:
+        print("ERROR: Send email failed: " + str(e), file=sys.stderr)
+
+def log(errorMessage):
+    try:
+        file = open(logPath, "a")
+        
+        file.write(f"{errorMessage}.\n")
+       
+        file.close()
+    except FileNotFoundError:
+        print("ERROR: File does not exist.")
+    except IOError:
+        print("ERROR: File is not accessible.")
+
+def error(errorMessage, dateTimeStamp):
+    print(f"{errorMessage}")
+    log(f"FAILURE: {errorMessage}, {dateTimeStamp}")
+    sendEmail(errorMessage, dateTimeStamp)
+    
 def main():
     dateTimeStamp = datetime.now().strftime("%Y%m%d-%H%M%S")
     argCount = len(sys.argv)
     if argCount != 2:
-        print("ERROR: jobname missing from command line")
+        error("ERROR: jobname missing from command line", dateTimeStamp)
     else:
         jobName = sys.argv[1]
         if jobName not in jobs:
-            print(f"ERROR: jobname {jobName} not defined.")
+            error(f"ERROR: jobname {jobName} not defined.", dateTimeStamp) 
         else:   
-            srcPath = jobs[jobName]
-            if not os.path.exists(srcPath):
-                print(f"ERROR: Source path {srcPath} does not exist.")
-            else:
-                if not os.path.exists(dstPath):
-                    print(f"ERROR: Destination path {dstPath} does not exist.")
-                else: 
-                     srcDetails = pathlib.PurePath(srcPath)
-                     
-                     dstLoc = dstPath + "/" + srcDetails.name + "-" + dateTimeStamp
-                     
-                     if pathlib.Path(srcPath).is_dir():
-                        shutil.copytree(srcPath, dstLoc)
-                     else:
-                        shutil.copy2(srcPath, dstLoc)
-                     pass
+            for srcPath in jobs[jobName]:
+                if not os.path.exists(srcPath):
+                    print(f"ERROR: Source path {srcPath} does not exist.") #
+                else:
+                    if not os.path.exists(dstPath):
+                        error(f"ERROR: Destination path {dstPath} does not exist.", dateTimeStamp)
+                    else: 
+                        srcDetails = pathlib.PurePath(srcPath)
+                         
+                        dstLoc = f"{dstPath}/{srcDetails.name}-{dateTimeStamp}"
+                         
+                        if pathlib.Path(srcPath).is_dir():
+                            shutil.copytree(srcPath, dstLoc)
+                        else:
+                            shutil.copy2(srcPath, dstLoc)
+                            
+                        log(f"SUCCESS: Backed up {srcPath} to {dstLoc}, {dateTimeStamp}")
+                            
+                           
+                        
+                    
+                    
+                
                 
 if __name__ == '__main__':
     main()
--- backupcfg.py
+++ backupcfg.py
@@ -1,4 +1,14 @@
-jobs = {'job17':'/home/ec2-user/environment/ictprg302/file1.dat',
-        'job18':'/home/ec2-user/environment/ictprg302/dir1'}
+jobs = {'job17':['/home/ec2-user/environment/ictprg302/file1.dat',
+                 '/home/ec2-user/environment/ictprg302/file1.dat'],  
+        'job18':['/home/ec2-user/environment/ictprg302/dir1']}
         
 dstPath = '/home/ec2-user/environment/ictprg302/backups'
+
+logPath = '/home/ec2-user/environment/ictprg302/backups/backup.log'
+
+smtp = {"sender": "finianthehuman@gmail.com", # elasticemail.com verified sender
+"recipient": "30011020@students.sunitafe.edu.au", # elasticemail.com verified recipient
+"server": "smtp.elasticemail.com", # elasticemail.com SMTP server
+"port": 2525, # elasticemail.com SMTP port
+"user": "finianthehuman@gmail.com", # elasticemail.com user
+"password": "5715C1970550997146570067D1C83EEE7BAE"} # elasticemail.com password
